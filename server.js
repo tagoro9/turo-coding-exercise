15cb70680a9f06c62106db7574e96cb714499aa8
Victor <tagoro9@gmail.com>
2015-12-27 21:52:56 +0000

    chore(port): change env port variable name

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.get('*', function(req, res) {
   res.sendFile(path.join(__dirname, prefix + 'index.html'));
 });
 
-app.listen(process.env.OPENSHIFT_NODEJS_PORT || config.port, 'localhost', function(err) {
+app.listen(process.env.PORT || config.port, 'localhost', function(err) {
   if (err) {
     console.log(err);
     return;
